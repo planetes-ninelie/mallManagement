fa05652d9db39c5261bf13c5c65402eb1caf44bb
planetes <1870370674@qq.com>
2024-03-30 14:08:27 +0800

    build: v1.0.5

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,22 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from '@/App.vue'
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css'
 
-createApp(App).mount('#app')
+//忽略当前文件ts类型的检测否则有红色提示(打包会失败)
+//@ts-ignore 
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import 'virtual:svg-icons-register'
+
+import globalComponent from '@/components'
+import '@/styles/index.scss'
+import router from '@/router'
+
+const app = createApp(App)
+app.use(ElementPlus, {
+    locale: zhCn
+})
+
+app.use(globalComponent)
+app.use(router)
+app.mount('#app')
