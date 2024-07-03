396b3bafe750e03934dab79c8f7298c281a9d175
planetes-ninelie <1870370674@qq.com>
2024-07-04 02:41:16 +0800

    build: v2.0.2 解决跨域代理

---
+++ docs/assets/index-B74kTAkG.js
@@ -0,0 +1,1 @@
+import{M as a}from"./index-30gCdVqT.js";const r=(e,t,l)=>a.get(`/admin/acl/role/${e}/${t}/?roleName=${l}`),s=e=>e.id?a.put("/admin/acl/role/update",e):a.post("/admin/acl/role/save",e),d=e=>a.delete("/admin/acl/role/remove/"+e),n=e=>a.delete("/admin/acl/role/batchRemove",{data:e});export{s as a,n as b,r as g,d as r};
---
+++ server.cjs
@@ -0,0 +1,25 @@
+//引入express
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+//配置端口号
+const POST = 8086
+// 创建一个app服务器实例
+const app = express()
+
+//配置静态资源
+app.use(express.static(__dirname + '/docs'))
+
+app.use(
+  createProxyMiddleware({
+    target: 'http://sph-api.atguigu.cn/',
+    changeOrigin: true,
+    pathFilter: '/api',
+    //路径重写
+    pathRewrite: {'^/api' : ''}
+  })
+);
+
+//绑定端口监听
+app.listen(POST,() => {
+  console.log(`本地服务器启动！http://localhost:${POST}`);
+})
